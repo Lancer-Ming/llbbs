5eaa7e947be0f80ed058b3c59b64f1d1a5b16a6d
ll <634019392@qq.com>
2018-06-17 00:05:56 +0800

    User can create toipc

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use App\Observers\TopicObserver;
+use App\Models\Topic;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -16,6 +17,8 @@ class AppServiceProvider extends ServiceProvider
         //
 
         \Carbon\Carbon::setLocale('zh');
+
+        Topic::observe(new TopicObserver());
     }
 
     /**
