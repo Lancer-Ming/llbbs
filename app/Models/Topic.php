912e43f596f0acc580c5b436b0a6ec07043b6f6c
ll <634019392@qq.com>
2018-06-17 17:31:13 +0800

    Auto translate slug

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -42,7 +42,7 @@ class Topic extends Model
 
     public function scopeRecentReplied($query)
     {
-        // 当话题有新回复时，我们将编写逻辑来更新话题模型的 reply_count 属性，
+        // 当话题有新回复时，我们to将编写逻辑来更新话题模型的 reply_count 属性，
         // 此时会自动触发框架对数据模型 updated_at 时间戳的更新
         return $query->orderBy('updated_at', 'desc');
     }
@@ -53,4 +53,8 @@ class Topic extends Model
         return $query->orderBy('created_at', 'desc');
     }
 
+    public function link($params = [])
+    {
+        return route('topics.show', array_merge([$this->id, $this->slug], $params));
+    }
 }
